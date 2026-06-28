6c0065721d20ed35915f76d32b4eeb99ad725128
arpayid <arjunwijaya585@gmail.com>
2026-06-29 01:50:00 +0800

    refactor: import core addressing in population module

--- apps/api/src/modules/population/population.module.ts
+++ apps/api/src/modules/population/population.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AddressingModule } from '../../core/addressing/addressing.module';
 import { PopulationService } from './population.service';
 import { PopulationController } from './population.controller';
 
 @Module({
+  imports: [AddressingModule],
   controllers: [PopulationController],
   providers: [PopulationService],
   exports: [PopulationService],
